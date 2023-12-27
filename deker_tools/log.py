8c5fc5462b95ab757da46c802f723ac489d7689a
matveyvarg <matveyvarg@gmail.com>
2023-12-27 15:40:43 +0100

    logger tool

---
+++ deker_tools/log.py
@@ -0,0 +1,52 @@
+import logging
+from typing import NamedTuple, List
+
+
+class LoggerNode(NamedTuple):
+    name: str
+    logger: logging.Logger
+    children: List['LoggerNode']
+
+
+def tree() -> LoggerNode:
+    """Return a tree of tuples representing the logger layout.
+
+    Each tuple looks like ``('logger-name', <Logger>, [...])`` where the
+    third element is a list of zero or more child tuples that share the
+    same layout.
+
+    """
+    root = LoggerNode('', logging.root, [])
+    nodes = {}
+    items = list(logging.root.manager.loggerDict.items())  # for Python 2 and 3
+    items.sort()
+    for name, logger in items:
+        nodes[name] = node = LoggerNode(name, logger, [])
+        i = name.rfind('.', 0, len(name) - 1)  # same formula used in `logging`
+        if i == -1:
+            parent = root
+        else:
+            parent = nodes[name[:i]]
+        parent[2].append(node)
+    return root
+
+
+def set_logger(format_string: str):
+    """Set format for all loggers in the tree.
+
+    :param format_string: Which format we set.
+    """
+    loggers = tree()
+    fmt = logging.Formatter(fmt=format_string)
+
+    def set_format_for_loggers(node: LoggerNode):
+        name, logger, children = node
+        if not isinstance(logger, logging.PlaceHolder):
+            for handler in logger.handlers:
+                handler.setFormatter(fmt)
+
+        for node in children:
+            set_format_for_loggers(node)
+
+    logging.basicConfig(format=format_string)
+    set_format_for_loggers(loggers)
